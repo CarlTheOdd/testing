089d7f438b2cca8c59681086171ee99f5655c637
CarlTheOdd <160284622+CarlTheOdd@users.noreply.github.com>
2024-02-29 19:40:01 -0500

    Update saves.js

--- js/saves.js
+++ js/saves.js
@@ -6,10 +6,39 @@ function E(x){
     return new ExpantaNum(x);
 }
 
+function notate(ex, acc=2) {
+    ex = E(ex)
+    let e = ex.log10().floor()
+    if (e.lt(6)) {
+        if (e.lt(3)) {
+            return ex.toFixed(acc)
+        }
+        return ex.floor().toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
+    let m = ex.div(E(10).pow(e))
+    return (e.log10().gte(6)?'':m.toFixed(2))+'e'+notate(e,0)
+}
+
 var player = {
-    quarks: 0,
+    quarks: E(0),
 
     gain: {
-        qpc: 1
+        qpc: E(1)
     }
+}
+
+function save(){
+    localStorage.setItem("incQuarksSave", btoa(JSON.stringify(player)));
+}
+
+function load(x){
+    if(typeof x == "string"){
+        let load = JSON.parse(atob(x));
+        player.quarks = ex(load.quarks);
+    }
+}
+
+function loadGame(){
+    load(localStorage.getItem("incQuarksSave"));
+    setInterval(save, 60000);
 }
